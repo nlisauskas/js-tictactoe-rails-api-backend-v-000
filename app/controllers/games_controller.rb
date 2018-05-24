97ea53c599149f2f81f85ded944f71330bc1d644
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-05-24 10:23:27 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,16 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+  
 
 end
